7d012a7c99c899fec678e48d100f7171972048db
pravinrajput1911-ops <pravinrajput1911@gmail.com>
2026-07-15 08:45:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// Welcome Message
+console.log("🚀 Smart Tools Hub Loaded");
 
+// Explore Button
+const btn = document.querySelector("button");
+
+if (btn) {
+  btn.addEventListener("click", function () {
+    alert("🚀 Welcome to Smart Tools Hub!\nMore powerful tools are coming soon.");
+  });
+}
